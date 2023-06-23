8e03032c613b5ce188a2404b1266998889e26cf8
aritradey-CS <81703791+aritradey-CS@users.noreply.github.com>
2023-06-23 21:46:51 +0530

    working with the designs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 const MenuItem = ({ title, selected, onClick }) => {
   return (
-    <button className={`menu-item ${selected ? 'selected' : ''}`} onClick={onClick}>
+    <button
+      className={`menu-item ${selected ? "selected" : ""}`}
+      onClick={onClick}
+    >
       {title}
     </button>
   );
@@ -12,28 +15,28 @@ const MenuItem = ({ title, selected, onClick }) => {
 const Content = ({ selectedItem }) => {
   const getContent = () => {
     switch (selectedItem) {
-      case 'home':
+      case "home":
         return (
           <div>
             <h2>Welcome to the Home Page</h2>
             <p>This is the content for the Home Page.</p>
           </div>
         );
-      case 'contact':
+      case "contact":
         return (
           <div>
             <h2>Contact Information</h2>
             <p>This is the content for the Contact Page.</p>
           </div>
         );
-      case 'information':
+      case "information":
         return (
           <div>
             <h2>Information Page</h2>
             <p>This is the content for the Information Page.</p>
           </div>
         );
-      case 'guide':
+      case "guide":
         return (
           <div>
             <h2>User Guide</h2>
@@ -49,7 +52,7 @@ const Content = ({ selectedItem }) => {
 };
 
 const App = () => {
-  const [selectedItem, setSelectedItem] = useState('home');
+  const [selectedItem, setSelectedItem] = useState("home");
 
   const handleMenuItemClick = (menuItem) => {
     setSelectedItem(menuItem);
@@ -58,10 +61,26 @@ const App = () => {
   return (
     <div className="App">
       <div className="left-panel">
-        <MenuItem title="Home" selected={selectedItem === 'home'} onClick={() => handleMenuItemClick('home')} />
-        <MenuItem title="Contact" selected={selectedItem === 'contact'} onClick={() => handleMenuItemClick('contact')} />
-        <MenuItem title="Information" selected={selectedItem === 'information'} onClick={() => handleMenuItemClick('information')} />
-        <MenuItem title="Guide" selected={selectedItem === 'guide'} onClick={() => handleMenuItemClick('guide')} />
+        <MenuItem
+          title="Home"
+          selected={selectedItem === "home"}
+          onClick={() => handleMenuItemClick("home")}
+        />
+        <MenuItem
+          title="Contact"
+          selected={selectedItem === "contact"}
+          onClick={() => handleMenuItemClick("contact")}
+        />
+        <MenuItem
+          title="Information"
+          selected={selectedItem === "information"}
+          onClick={() => handleMenuItemClick("information")}
+        />
+        <MenuItem
+          title="Guide"
+          selected={selectedItem === "guide"}
+          onClick={() => handleMenuItemClick("guide")}
+        />
       </div>
       <Content selectedItem={selectedItem} />
     </div>
